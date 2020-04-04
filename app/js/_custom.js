28e8089a65a55058f37ca816c27941297ad78443
v-nakon <nakonechny.vl@hotmail.com>
2020-04-04 12:08:36 +0300

    refactoring category

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,13 +1,19 @@
-var searchName = document.querySelector("#event_name");
-var searchLocation = document.querySelector("#city");
+import { addOptionSelect } from "./helpers/help_create_elements.js";
+// import { createEventCard } from "./helpers/help_create_elements.js";
+import { getCategories } from "./helpers/requests.js";
+import { getCities } from "./helpers/requests.js";
+import { getSubjects } from "./helpers/requests.js";
+import { addCatToSearch } from "./helpers/category_search_block.js";
+
 var modalNotFound = document.querySelector(".container_modal_notfound");
 var closeModalNotfound = document.querySelector(".close_modal_notfound");
 var spinner = document.querySelector(".block_spinner");
-getCities("city");
-getCategories();
-getSubjects();
+// getCities("city");
+getCitiesData();
+getCategoriesData();
+getSubjectsData();
 
-closeModalNotfound.addEventListener("click", function() {
+closeModalNotfound.addEventListener("click", function () {
   modalNotFound.style.display = "none";
   document.location.href = "/";
 });
@@ -56,145 +62,50 @@ function createEventCard(objItem) {
   listEventsElement.append(eventCardElement);
 }
 
-function getCities(elementSelect) {
-  axios
-    .get("https://eventafisha.com/api/v1/cities")
-    .then(function(response) {
+function getCitiesData() {
+  getCities()
+    .then((response) => {
       for (let item in response.data) {
-        addOptionSelect(response.data[item], elementSelect);
+        addOptionSelect(response.data[item], "city");
       }
     })
-    .catch(function(error) {
-      // handle error
+    .catch((error) => {
       console.log(error);
-    })
-    .then(function() {
-      // always executed
     });
 }
-function getSubjects() {
-  axios
-    .get("https://eventafisha.com/api/v1/subjects")
-    .then(function(response) {
+function getSubjectsData() {
+  getSubjects()
+    .then((response) => {
       for (let item in response.data) {
         addOptionSelect(response.data[item], "subject_search");
       }
     })
-    .catch(function(error) {
-      // handle error
+    .catch((error) => {
       console.log(error);
-    })
-    .then(function() {
-      // always executed
     });
 }
-function getCategories() {
-  axios
-    .get("https://eventafisha.com/api/v1/categories")
-    .then(function(response) {
+function getCategoriesData() {
+  getCategories()
+    .then((response) => {
       for (let item in response.data) {
         addCatToSearch(response.data[item], ".container_category");
       }
     })
-    .catch(function(error) {
-      // handle error
+    .catch((error) => {
       console.log(error);
-    })
-    .then(function() {
-      // always executed
     });
-}
-// categories for search
-function addCatToSearch(item, elementTo) {
-  // console.log(item);
-  let catElements = document.querySelector(elementTo);
-  let newCat = document.createElement("div");
-  newCat.classList.add("item_category");
-  newCat.setAttribute("category_id", item.id);
-  newCat.innerHTML = item.title;
-  catElements.appendChild(newCat);
-  addEventToElement(newCat, item.id);
-}
-function addEventToElement(element, catId) {
-  element.addEventListener("click", function() {
-    delActiveColor();
-    console.log("CAT - ", catId);
-    element.classList.add("color_active_cat");
-    document.querySelector(".arrow_down").classList.add("color_active_cat");
-    categorySearch = catId;
-    paginationAjax(
-      "#pagination",
-      nameEventSearch,
-      cityEventSearch,
-      fromDateSearch,
-      toDateSearch,
-      categorySearch,
-      subjectSearch
-    );
-  });
-}
-function delActiveColor() {
-  let arrActiveColor = document.querySelectorAll(".color_active_cat");
-  console.log("arr", arrActiveColor);
-  arrActiveColor.forEach(function(el) {
-    el.classList.remove("color_active_cat");
-    // console.log("delete class")
-  });
-}
-// end categories for search
-function addOptionSelect(item, elementSelect) {
-  let selectCategory = document.getElementById(elementSelect);
-  let option = document.createElement("option");
-  option.value = item.id;
-  option.innerHTML = item.title;
-  selectCategory.add(option);
 }
 
 // search function
-var fromDateSearch = "";
-var toDateSearch = "";
-var nameEventSearch = "";
-var cityEventSearch = "";
-var categorySearch = "";
-var subjectSearch = "";
-var arrElCat = [
-  {
-    el: document.querySelector("#search_cat_all"),
-    id: ""
-  },
-  {
-    el: document.querySelector("#search_cat_main1"),
-    id: 24
-  },
-  {
-    el: document.querySelector("#search_cat_main2"),
-    id: 1
-  },
-  {
-    el: document.querySelector("#search_cat_main3"),
-    id: 2
-  }
-];
-function addListenerToArrEl(arr) {
-  for (let i in arr) {
-    arr[i].el.addEventListener("click", function() {
-      delActiveColor();
-      arr[i].el.classList.add("color_active_cat");
-      categorySearch = arr[i].id;
-      paginationAjax(
-        "#pagination",
-        nameEventSearch,
-        cityEventSearch,
-        fromDateSearch,
-        toDateSearch,
-        categorySearch,
-        subjectSearch
-      );
-    });
-  }
+export var fromDateSearch = "";
+export var toDateSearch = "";
+export var nameEventSearch = "";
+export var cityEventSearch = "";
+export var categorySearch = "";
+export var subjectSearch = "";
+export function setCategorySearch(value) {
+  categorySearch = value;
 }
-addListenerToArrEl(arrElCat);
-
 function splitSearchDate(dates) {
   if (dates.indexOf(" - ") > -1) {
     let dateArr = dates.split(" - ");
@@ -226,7 +137,7 @@ function removeEventList() {
   }
 }
 
-$(function() {
+$(function () {
   paginationAjax("#pagination", "", "", "", "", "", "");
 });
 
@@ -253,7 +164,7 @@ function checkSearchParam(title, city, dateStart, dateEnd, category, subject) {
   return link;
 }
 
-function paginationAjax(
+export function paginationAjax(
   name,
   title,
   city,
@@ -274,7 +185,7 @@ function paginationAjax(
   container.pagination({
     dataSource: url,
     locator: "data",
-    totalNumberLocator: function(dataSource) {
+    totalNumberLocator: function (dataSource) {
       // you can return totalNumber by analyzing response content
       console.log("test", dataSource.total);
       return dataSource.total;
@@ -289,15 +200,15 @@ function paginationAjax(
     className: "paginationjs-theme paginationjs-small",
     alias: {
       pageNumber: "page",
-      pageSize: "limit"
+      pageSize: "limit",
     },
     ajax: {
-      beforeSend: function() {
+      beforeSend: function () {
         // container.prev().html('Загрузка данных');
         spinner.classList.remove("hide_spinner");
-      }
+      },
     },
-    callback: function(response, pagination) {
+    callback: function (response, pagination) {
       // window.console && console.log(22, response, pagination.pageNumber);
       // console.log(pagination.pageNumber);
       // console.log("res len", response.length);
@@ -306,7 +217,7 @@ function paginationAjax(
         modalNotFound.style.display = "block";
       } else {
         removeEventList();
-        $.each(response, function(index, item) {
+        $.each(response, function (index, item) {
           createEventCard(item);
         });
         window.scrollTo(0, 0);
@@ -319,7 +230,7 @@ function paginationAjax(
       // 	searchNameMob.value = "";
       // 	searchLocationMob.value = "";
       // }
-    }
+    },
   });
 }
 
@@ -340,43 +251,15 @@ let $btn = $(".datepicker_btn"),
       showEvent: "none",
       range: true,
 
-      onSelect: function(dateText, inst) {
+      onSelect: function (dateText, inst) {
         splitSearchDate(dateText);
       },
-      minDate: new Date()
+      minDate: new Date(),
     })
     .data("datepicker");
 
-$btn.on("click", function() {
+$btn.on("click", function () {
   dp.show();
   dp.clear();
   $input.focus();
 });
-
-// Open the dropdown window CATEGORY
-var catWindow = document.querySelector(".dropdown_content");
-var containerCatWindow = document.querySelector(".container_category");
-var btnShowCat = document.querySelector(".dropbtn");
-
-//   btnShowCat.addEventListener("click", function() {
-//     catWindow.style.display = "block";
-//   });
-btnShowCat.addEventListener("click", function() {
-  catWindow.classList.toggle("show");
-});
-// Close the dropdown if the user clicks outside of it CATEGORY
-window.onclick = function(event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdown = document.querySelector(".dropdown_content");
-    if (dropdown.classList.contains("show")) {
-      dropdown.classList.remove("show");
-    }
-  }
-};
-//  window.onclick = function(event) {
-//    if (!event.target.matches('.dropbtn') && !event.target.matches('.container_category') && !event.target.matches('.item_category')) {
-// 	   if (catWindow.classList.contains('show')) {
-// 		   catWindow.classList.remove('show');
-// 	   }
-//    }
-//  };
